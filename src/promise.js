69bd8e723539bac3ca7307325a879fbdd17a1a65
Denis Derkach <podstavka128@gmail.com>
2019-01-10 11:11:37 +0200

    Create the Promise Constructor and constants

--- src/promise.js
+++ src/promise.js
@@ -1,5 +1,36 @@
-class OwnPromise {
+const RESOLVED = 'RESOLVED';
+const REJECTED = 'REJECTED';
+const PENDING = 'PENDING';
 
+let cb = null;
+
+class OwnPromise {
+  constructor(executor) {
+    this.state = PENDING;
+    this.callbacks = [];
+
+    const resolve = data => {
+      if (this.state !== PENDING) {
+        return;
+      }
+
+      this.state = RESOLVED;
+      this.value = data;
+      this.callbacks.forEach(({ res, rej }) => {
+        this.value = res(this.value);
+      });
+    }; // Контекстом будет этот промис, поэтому не function()
+
+    const reject = error => {
+
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e);
+    }
+  }
 }
 
 module.exports = OwnPromise;
