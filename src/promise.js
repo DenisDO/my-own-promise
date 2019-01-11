2fc50696d2118a599cc97a2a01baa1ead87b1297
Denis Derkach <podstavka128@gmail.com>
2019-01-11 16:19:13 +0200

    Create easy method ALL realization

--- src/promise.js
+++ src/promise.js
@@ -107,20 +107,78 @@ class OwnPromise {
     return new OwnPromise((resolve, reject) => reject(error));
   }
 
+  static all(iterable) {
+    if (typeof this !== 'function') {
+      throw new TypeError('this is not a constructor');
+    }
+
+    return new OwnPromise((resolve, reject) => {
+      const isIterable = object => object !== null && typeof object[Symbol.iterator] === 'function';
+
+      if (!isIterable(iterable)) {
+        console.log('NON-ITERABLE');
+        throw new TypeError('Non-iterable value');
+      }
+
+      const isEmptyIterable = iterable => {
+        for (let key of iterable) {
+          return true;
+        }
+        return false;
+      };
+
+      if (!(isEmptyIterable(iterable))) {
+        console.log('EMPTY');
+        return resolve([]);
+      }
+
+      const result = [];
+
+      for (let i = 0; i < iterable.length; i++) {
+        if (iterable[i].state === RESOLVED) {
+          result.push(iterable[i].value);
+        } else if (iterable[i].state === REJECTED) {
+          console.log(iterable[i].value);
+        } else {
+          resolve(iterable[i]);
+        }
+      }
+
+      console.log(result);
+    });
+  }
+
   catch(rej) {
     return this.then(undefined, rej);
   }
 }
 
-module.exports = OwnPromise;
+// module.exports = OwnPromise;
 
 const p1 = new OwnPromise(function(resolve, reject) {
+  resolve(1);
+});
+
+const p2 = new OwnPromise(function(resolve, reject) {
+  resolve(2);
+});
+
+const p3 = new OwnPromise(function(resolve, reject) {
   resolve(3);
 });
 
-p1
-.then(data => {console.log(data); return 5;})
-.then(data => {console.log(data);});
+const p4 = new OwnPromise(function(resolve, reject) {
+  resolve(4);
+});
+
+// p1
+// .then(data => {console.log('1', data); return 5;})
+// .then(data => {console.log('3', data);});
+
+// p1
+// .then(data => {console.log('2', data);});
+
+OwnPromise.all([p1, p2, p3, p4]);
 
 // const p = new OwnPromise(function(resolve, reject) {
 //   setTimeout(() => {
